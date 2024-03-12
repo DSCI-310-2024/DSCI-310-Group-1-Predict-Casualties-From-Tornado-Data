a8d2c1fcf8b2dfa76d74ebc35ed12a6d885379aa
Erika <lordoftherings20179cws17@gmail.com>
2024-03-12 16:42:48 -0700

    added supress messages

--- src/01_download_data.r
+++ src/01_download_data.r
@@ -2,7 +2,7 @@
 
 "This script downloads the data from a specified URL and saves it to a specified local filepath.
 
-Usage: Rscript src/01_download_data.R --url=<url> 
+Usage: Rscript src/01_download_data.r --url=<url> 
 
 Options:
 --url=<url>               URL of the dataset location from the web or local file path
@@ -16,10 +16,11 @@ opt <- docopt(doc)
 main <- function(url) {
 
     # read in data from web
+    # https://raw.githubusercontent.com/rfordatascience/tidytuesday/a9e277dd77331e9091e151bb5adb584742064b3e/data/2023/2023-05-16/tornados.csv 
     data <- read_csv(url)
 
     # save the file in specified local filepath
-    write_csv(data, 'data/raw/raw_tornado_data.csv')
+    write_csv(data, "data/raw/raw_tornado_data.csv")
 }
 
 main(opt$url)
