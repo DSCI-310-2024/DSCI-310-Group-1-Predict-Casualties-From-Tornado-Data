88f628e93f0f7cdb2bc8d00c0ca76084d2c99ea2
Erika <lordoftherings20179cws17@gmail.com>
2024-03-13 14:50:25 -0700

    reverted back changes that I removed

--- src/01_download_data.r
+++ src/01_download_data.r
@@ -2,10 +2,11 @@
 
 "This script downloads the data from a specified URL and saves it to a specified local filepath.
 
-Usage: Rscript src/01_download_data.r --url=<url> 
+Usage: Rscript src/01_download_data.r --url=<url> --file_path=<file_path>
 
 Options:
 --url=<url>               URL of the dataset location from the web or local file path
+--file_path=<file_path>   The local filepath where you want to save the file and what to name it
 " -> doc
 
 suppressMessages(library(tidyverse))
@@ -13,14 +14,14 @@ suppressWarnings(library(docopt))
 
 opt <- docopt(doc)
 
-main <- function(url) {
+main <- function(url, file_path) {
 
     # read in data from web
     # https://raw.githubusercontent.com/rfordatascience/tidytuesday/a9e277dd77331e9091e151bb5adb584742064b3e/data/2023/2023-05-16/tornados.csv 
     data <- read_csv(url)
 
     # save the file in specified local filepath
-    write_csv(data, "data/raw/raw_tornado_data.csv")
+    write_csv(data, file_path)
 }
 
-main(opt$url)
+main(opt$url, opt$file_path)
