f281712cf904a4042afcf238774b46cfd60cafaa
Riddha T <riddha.t@gmail.com>
2024-03-14 02:53:36 -0700

    Fixed file numbering and added outlierless plots

--- src/05_linear_model_results.R
+++ src/05_linear_model_results.R
@@ -1,17 +1,13 @@
-"This script reads the raw data and performs any necessary preprocessing
-before the exploratory data analysis or modelling takes place. It also
-splits the processed data into train and test sets.
-
-This script creates takes the test data and linear regression model and uses
+"This script creates takes the test data and linear regression model and uses
 them to create scatterplots used to assess the accuracy of the regression model.
 Additionally, this script creates boxplots from the test data to assess the 
 outliers in the data set.
 
-Usage: 05_linear_model_results.R --test_data=<test_data> --lm_fit=<lm_fit>
+Usage: 05_linear_model_results.R --test_data=<test_data> --lin_fit=<lin_fit>
 
 Options:
 --test_data=<test_data>   Path to the test data
---lm_fit=<lm_fit>         Linear regression model data
+--lin_fit=<lin_fit>         Linear regression model data
 " -> doc
 
 library(tidyverse)
@@ -20,22 +16,40 @@ library(docopt)
 
 opt <- docopt(doc)
 
-main <- function(test_data, lm_fit) {
+main <- function(test_data, outlierless_train, outlierless_test, lin_fit) {
   
   # Read test data
   test_df <- read_csv(test_data)
+  
+  # Read linear regression model
+  lm_fit <- readRDS(lin_fit)
   
   # Make and store the linear model scores table
   lm_test_scores_table <- lm_fit |>
     metrics(truth = fatalities, estimate = .pred)
   
   write_csv(lm_test_scores_table, 
-            file.path("results/02_linear_model_test_scores.csv"))
+            file.path("results/02_linear_model_test_scores_table.csv"))
   
   # Draw linear model onto graphs
   fatal_predictions <- lm_fit |>
     predict(test_df) |>
     bind_cols(test_df)
+  
+  y_intercept <- tidy(lm_fit) |>
+    filter(term == "(Intercept)") |>
+    select(estimate) |>
+    pull()
+  
+  tornado_length <- tidy(lm_fit) |>
+    filter(term == "length") |>
+    select(estimate) |>
+    pull()
+  
+  tornado_width <- tidy(lm_fit) |>
+    filter(term == "width") |>
+    select(estimate) |>
+    pull()
   
   options(repr.plot.width = 8, repr.plot.height = 7)
   
@@ -47,7 +61,7 @@ main <- function(test_data, lm_fit) {
     scale_x_continuous(trans = "log10") + 
     xlab("Actual Number of Fatalities") +
     ylab("Predicted Number of Fatalities") +
-    ggtitle("Figure 4: Actual Number of Fatalities vs Predicted Number of Fatalities") +
+    ggtitle("Actual Number of Fatalities vs Predicted Number of Fatalities") +
     theme(text = element_text(size = 14), plot.title = element_text(hjust = 0.5))
   
   ggsave("results/03_actual_vs_predicted_fatalities_plot.png", fatal_model_viz)
@@ -56,11 +70,11 @@ main <- function(test_data, lm_fit) {
   ## Note: Blue line represents predicted values based on our regression model when tornado length = 0
   fatal_widths_plot <- ggplot(test_df, aes(x = width, y = fatalities)) +
     geom_point(alpha = 0.4) +
-    geom_abline(intercept = -0.1183680, slope = 0.0007337, color = "steelblue", linewidth = 2) +
+    geom_abline(intercept = y_intercept, slope = tornado_width, color = "steelblue", linewidth = 2) +
     scale_y_continuous(trans = "log10") + 
     xlab("Tornado Width (Yards)") +
     ylab("Fatalities") +
-    ggtitle("Figure 5: Fatalities vs Width Plot") +
+    ggtitle("Fatalities vs Width Plot") +
     theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
   
   ggsave("results/04_fatalities_vs_width_plot.png", fatal_widths_plot)
@@ -69,14 +83,14 @@ main <- function(test_data, lm_fit) {
   ## Note: Blue line represents predicted values based on our regression model when tornado width = 0
   fatal_length_plot <- ggplot(test_df, aes(x = length, y = fatalities)) +
     geom_point(alpha = 0.4) +
-    geom_abline(intercept = -0.1183680, slope = 0.0367960, color = "steelblue", linewidth = 2) +
+    geom_abline(intercept = y_intercept, slope = tornado_length, color = "steelblue", linewidth = 2) +
     scale_y_continuous(trans = "log10") + 
     xlab("Tornado Length (Miles)") +
     ylab("Fatalities") +
-    ggtitle("Figure 6: Fatalities vs Length Plot") +
+    ggtitle("Fatalities vs Length Plot") +
     theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
   
-  ggsave("results/04_fatalities_vs_length_plot.png", fatal_length_plot)
+  ggsave("results/05_fatalities_vs_length_plot.png", fatal_length_plot)
   
   # Create and store boxplot for tornado widths
   width_boxplot <- ggplot(test_df, aes(y = width)) +
@@ -102,6 +116,96 @@ main <- function(test_data, lm_fit) {
   
   ggsave("results/08_fatalities_outlier_boxplot.png", fatalities_boxplot)
   
+  ############
+  ############ The following could be added to script_4 as the code is identical
+  
+  # Reading the outlierless training data
+  train_data <- read_csv(outlierless_train)
+  
+  # Create linear model and recipe
+  lm_spec <- linear_reg() %>%
+    set_engine("lm") %>%
+    set_mode("regression")
+  
+  lm_recipe <- recipe(fatalities ~ width + length, data = train_data)
+  
+  # Fit linear model and save to RDS
+  outlierless_lm_fit <- workflow() %>%
+    add_recipe(lm_recipe) %>%
+    add_model(lm_spec) %>%
+    fit(data = train_data)
+  
+  ############
+  ############
+  
+  # Read the outlierless testing data
+  outlierless_test_df  <- read.csv(outlierless_test)
+  
+  # Make and store the linear model scores table
+  outlierless_lm_test_scores_table <- outlierless_lm_fit |>
+    metrics(truth = fatalities, estimate = .pred)
+  
+  write_csv(outlierless_lm_test_scores_table, 
+            file.path("results/09_linear_model_test_scores_without_outliers_table.csv"))
+  
+  # Draw linear model onto graphs without outliers
+  outlierless_fatal_predictions <- lm_fit |>
+    predict(outlierless_test_df) |>
+    bind_cols(outlierless_test_df)
+  
+  outlierless_intercept <- tidy(outlierless_lm_fit) |>
+    filter(term == "(Intercept)") |>
+    select(estimate) |>
+    pull()
+  
+  outlierless_tornado_length <- tidy(outlierless_lm_fit) |>
+    filter(term == "length") |>
+    select(estimate) |>
+    pull()
+  
+  outlierless_tornado_width <- tidy(outlierless_lm_fit) |>
+    filter(term == "width") |>
+    select(estimate) |>
+    pull()
+  
+  # Create and store scatter plot comparing accuracy of the linear model without outliers
+  ## Note: Red line represents where points would be plotted if the model was 100% accurate
+  outlierless_model_viz <- ggplot(outlierless_fatal_predictions, aes(x = fatalities, y = .pred)) +
+    geom_point(alpha = 0.6) +
+    geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed", linewidth = 2)+
+    scale_x_continuous(trans = "log10") + 
+    xlab("Actual Number of Fatalities") +
+    ylab("Predicted Number of Fatalities") +
+    ggtitle("Actual Number of Fatalities vs Predicted Number of Fatalities") +
+    theme(text = element_text(size = 14), plot.title = element_text(hjust = 0.5))
+  
+  ggsave("results/10_actual_vs_predicted_fatalities_plot_no_outliers.png", outlierless_model_viz)
+  
+  # Create and store scatter plot for tornado width with linear regression line
+  ## Note: Blue line represents predicted values based on our regression model when tornado length = 0
+  outlierless_widths_plot <- ggplot(outlierless_test_df, aes(x = width, y = fatalities)) +
+    geom_point(alpha = 0.4) +
+    geom_abline(intercept = outlierless_intercept, slope = outlierless_tornado_width, color = "steelblue", linewidth = 2) +
+    scale_y_continuous(trans = "log10") + 
+    xlab("Tornado Width (Yards)") +
+    ylab("Fatalities") +
+    ggtitle("Fatalities vs Width Plot") +
+    theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
+  
+  ggsave("results/11_fatalities_vs_width_plot_no_outliers.png", outlierless_widths_plot)
+  
+  # Create and store scatter plot for tornado length with linear regression line
+  ## Note: Blue line represents predicted values based on our regression model when tornado width = 0
+  outlierless_length_plot <- ggplot(outlierless_test_df, aes(x = length, y = fatalities)) +
+    geom_point(alpha = 0.4) +
+    geom_abline(intercept = outlierless_intercept, slope = outlierless_tornado_length, color = "steelblue", linewidth = 2) +
+    scale_y_continuous(trans = "log10") + 
+    xlab("Tornado Length (Miles)") +
+    ylab("Fatalities") +
+    ggtitle("Fatalities vs Length Plot") +
+    theme(text = element_text(size = 15), plot.title = element_text(hjust = 0.5))
+  
+  ggsave("results/12_fatalities_vs_length_plot_no_outliers.png", outlierless_length_plot)
 }
 
-main(opt$test_data, opt$lm_fit)
+main(opt$test_data, opt$outlierless_train, opt$outlierless_test, opt$lin_fit)
