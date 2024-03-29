8d52c1f9f28abe678583f2d5bc35792ef1928ae2
Riddha T <riddha.t@gmail.com>
2024-03-29 11:57:07 -0700

    Created test boxplot svg file

--- tests/testthat/helper-create_boxplot.R
+++ tests/testthat/helper-create_boxplot.R
@@ -3,7 +3,8 @@
 library(ggplot2)
 source("../../R/boxplot_viz.R")
 
-test_boxplot_data <- data.frame("fatalities" = c(0, 0, 1, 1, 2, 2, 5, 10, 15))
+test_boxplot_data <- data.frame("fatalities" = c(0, 0, 1, 1, 2, 2, 5, 10, 15), 
+                                "magnitude" = c(1, 2, 3, 4, 5, 6, 7, 8, 9))
 
 test_boxplot <- boxplot_viz(test_boxplot_data, fatalities) + labs(x = "Fatalities",  
                title = "Boxplot of Fatalities")
