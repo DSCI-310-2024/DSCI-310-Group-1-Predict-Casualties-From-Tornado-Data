97ac829ea528b536af61f6e461b990fa9f502e5b
Riddha T <riddha.t@gmail.com>
2024-03-30 14:02:59 -0700

    Made boxplot_viz test checking dataframe robust

--- tests/testthat/test-boxplot_viz.R
+++ tests/testthat/test-boxplot_viz.R
@@ -16,5 +16,6 @@ test_that("'col' should be a column name in the data frame", {
 
 test_that("`data` should be a data frame", {
   expect_error(boxplot_viz(list_input, fatalities))
+  expect_error(boxplot_viz("string", fatalities))
 })
 
