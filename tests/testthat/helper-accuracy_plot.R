550be9e1f474717b5ef4fb809799ad7112eca49a
Riddha T <riddha.t@gmail.com>
2024-03-30 20:13:34 -0700

    Fixed bugs data frame bug in helper-accuracy_plot

--- tests/testthat/helper-accuracy_plot.R
+++ tests/testthat/helper-accuracy_plot.R
@@ -6,13 +6,11 @@ library(tidymodels)
 source("../../R/accuracy_plot.R")
 source("../../R/fit_linear_model.R")
 
-lin_model_data <- tibble(
-  x = 1:10,
-  z = 1:10,
-  y = 2 * (1:10) + 1 * (1:10)
-)
+lin_model_data <- data.frame('x' = c(1, 2, 1, 1, 2, 3, 6, 5, 7, 4), 
+           'y' = c(1, 2, 3, 1, 2, 3, 1, 2, 3, 1),
+           'z' = c(3, 6, 5, 3, 6, 9, 13, 12, 17, 9))
 
-lm_data <- fit_linear_model(y ~ x + z, lin_model_data) |>
+lm_data <- fit_linear_model(z ~ x + y, lin_model_data) |>
     predict(lin_model_data) |>
     bind_cols(lin_model_data)
 
